16322a4cf83e5d8e0ebc331bb7004212377397f6
Denis Bystruev <dbystruev@me.com>
2019-04-13 16:27:58 +0300

    Implemented data source

---
+++ Universal Reference/View/ImageCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImageCell.swift
+//  Universal Reference
+//
+//  Created by Denis Bystruev on 13/04/2019.
+//  Copyright © 2019 Denis Bystruev. All rights reserved.
+//
+
+import UIKit
+
+class ImageCell: UITableViewCell {
+    @IBOutlet weak var customImageView: UIImageView!
+}
---
+++ Universal Reference/View/ListCell.swift
@@ -0,0 +1,13 @@
+//
+//  ListCell.swift
+//  Universal Reference
+//
+//  Created by Denis Bystruev on 13/04/2019.
+//  Copyright © 2019 Denis Bystruev. All rights reserved.
+//
+
+import UIKit
+
+class ListCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+}
